7d6ed4b9dac3c9caa1d4dc1fd70eaf5550c01e4c
Your Name <ahmedragheb75@gmail.com>
2018-04-22 20:02:50 +0300

    23 charfield & textfield

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,10 @@ from django.db import models
 # Create your models here.
 
 class PostModel(models.Model):
-	# id = models.AutoField(primary_key=True)
-	# id = models.IntegerField(primary_key=True) # auto increments 1, 2, 3, 4,
+	id 			= models.BigAutoField(primary_key=True)
+	active 		= models.BooleanField(default=True)
+	title		= models.CharField(max_length=240)
+	content		= models.TextField(null=True,blank=True)
+
 
 	pass
