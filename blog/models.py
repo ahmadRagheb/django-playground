3c7bf67dc91ab3f148fcb2530b04962a6fa25618
Your Name <ahmedragheb75@gmail.com>
2018-04-22 20:36:39 +0300

    27 built in validation

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.encoding import smart_text
+from django.utils import timezone
 # Create your models here.
 
 PUBLISH_CHOICES = [
@@ -9,17 +10,21 @@ PUBLISH_CHOICES = [
 	]
 
 class PostModel(models.Model):
-	id 			= models.BigAutoField(primary_key=True)
-	active 		= models.BooleanField(default=True)
-	title		= models.CharField(max_length=240, verbose_name='Post title')
-	content		= models.TextField(null=True,blank=True)
-	publish 	= models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+	id 				= models.BigAutoField(primary_key=True)
+	active 			= models.BooleanField(default=True)
+	title			= models.CharField(max_length=240, verbose_name='Post title', unique=True)
+	content			= models.TextField(null=True,blank=True)
+	publish 		= models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+	view_count		= models.IntegerField(default=0)
+	publish_date	= models.DateField(auto_now=False, auto_now_add=False, default=timezone)
+
+
 	class meta:
 		verbose_name = 'Post'
 		verbose_name_plural = 'Posts'			
+		# unique_together = [('title','slug')]
 
 	#smart_text method for unicode whatever language we are using no errors
-	
 	def __unicode__(self): #python 2
 		return smart_text(self.title)
 
