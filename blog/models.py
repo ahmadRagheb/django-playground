60d1c783ac7bf50aa90a15c4d48c987e68cb3399
ahmed <ahmedragheb75@gmail.com>
2018-04-22 16:09:42 +0300

    second commit

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+# Create your models here.
+
+class PostModel(models.Model):
+	# id = models.AutoField(primary_key=True)
+	# id = models.IntegerField(primary_key=True) # auto increments 1, 2, 3, 4,
+
+	pass
