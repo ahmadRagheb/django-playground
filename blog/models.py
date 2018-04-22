73010a0b1badb012347d86f7cef5c4279df60658
Your Name <ahmedragheb75@gmail.com>
2018-04-22 20:21:22 +0300

    25 Choices fild

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 # Create your models here.
 
+PUBLISH_CHOICES = [
+		('draft','Draft'),
+		('publish','Publish'),
+		('private','Private'),		
+	]
+
 class PostModel(models.Model):
 	id 			= models.BigAutoField(primary_key=True)
 	active 		= models.BooleanField(default=True)
 	title		= models.CharField(max_length=240, verbose_name='Post title')
 	content		= models.TextField(null=True,blank=True)
-
+	publish 	= models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
 	class meta:
 		verbose_name = 'Post'
 		verbose_name_plural = 'Posts'			
